ffae9d4b6604fb8f2b987cbd20152171fc769745
KarukuuvelRaj <51171356+KarukuuvelRaj@users.noreply.github.com>
2022-09-17 12:02:57 +0530

    Add files via upload

---
+++ Week1/OddOrEven.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		int n=123;
+		if(n%2==0) {
+			System.out.println("Even");
+		}else {
+			System.out.println("Odd");
+		}
+	}
+
+}
